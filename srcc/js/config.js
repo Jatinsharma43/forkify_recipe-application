5a09897b6969d143419a46f60ccd66bc2582d016
Jatinsharma43 <jatin0237@gmail.com>
2025-02-16 17:13:07 +0530

    initial commit

---
+++ srcc/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8ae7fc5e-96d7-489d-8dc4-0934e74a19ce';
+export const MODAL_CLOSE_SEC = 2.5;
